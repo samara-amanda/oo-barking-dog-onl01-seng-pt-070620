a82dec5fbbf86c77c7fa43a891b2a658d55e018f
Samara Grant <samara.amanda01@gmail.com>
2020-07-21 04:13:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    my_dogs_name = dog_name
+  end
+
+  def name 
+    @my_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+lassie = Dog.new
+lassie.name
+
+
+    
